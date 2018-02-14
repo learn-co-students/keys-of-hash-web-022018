63729fdf3f44ceceb611fb57581bf027a5107dfe
lidianvilazio <lidianvilazio@hotmail.com>
2018-02-14 08:28:58 -0500

    Keys Of Hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+    def keys_of(*arguments)
+        # code goes here
+        a = []
+        self.select do |k, v|
+            arguments.select do |i|
+                if v == i
+                    a << k
+                end
+            end
+        end
+        a
+    end
 end
+
